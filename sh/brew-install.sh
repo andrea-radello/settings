8aca3f8bd7aeef4918297fc19b76df589569a37c
GibboK <gibbok@users.noreply.github.com>
2018-11-21 21:34:09 +0100

    Brew install script

---
+++ sh/brew-install.sh
@@ -0,0 +1,16 @@
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+# install formulas
+brew install git
+brew install nvm
+brew install ipfs
+brew install jq
+brew install vault
+brew install zsh
+brew install awscli
+brew install tmux
+
+# brew install vim --with-override-system-vi
