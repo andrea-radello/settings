fa663b39f03da2091f66104aea5009d1757602af
GibboK <gibbok@users.noreply.github.com>
2018-11-21 21:40:58 +0100

    Update script

--- sh/brew-install.sh
+++ sh/brew-install.sh
@@ -1,9 +1,9 @@
-# install brew
+#  brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 brew update
+brew tap caskroom/cask
 
-# install formulas
+# formulas
 brew install git
 brew install nvm
 brew install ipfs
@@ -14,3 +14,9 @@ brew install awscli
 brew install tmux
 
 # brew install vim --with-override-system-vi
+
+# apps
+brew cask install google-chrome
+brew cask install firefox
+brew cask install slack
+brew cask install zoomus
