d26e4cd1970bc86e9f27c51a3f727480cbd6abb9
GibboK <gibbok@users.noreply.github.com>
2018-11-21 21:54:54 +0100

    Update brew-install.sh

--- sh/brew-install.sh
+++ sh/brew-install.sh
@@ -4,32 +4,32 @@ brew update
 brew tap caskroom/cask
 
 # formulas
+brew install awscli
 brew install git
-brew install nvm
 brew install ipfs
 brew install jq
+brew install nvm
+brew install tmux
 brew install vault
 brew install zsh
-brew install awscli
-brew install tmux
 
 # brew install vim --with-override-system-vi
 
 # apps
-brew cask install google-chrome
+brew cask install adobe-creative-cloud
 brew cask install firefox
-brew cask install slack
-brew cask install zoomus
-brew cask install sourcetree
 brew cask install fontbase
+brew cask install google-chrome
 brew cask install iterm2
+brew cask install kdiff3
 brew cask install keepassx
-brew cask install obs
 brew cask install libreoffice
+brew cask install obs
 brew cask install skype
+brew cask install slack
+brew cask install sourcetree
+brew cask install spectacle
+brew cask install visual-studio-code
 brew cask install vlc
 brew cask install xscope
-brew cask install spectacle
-brew cask install adobe-creative-cloud
-brew cask install kdiff3
-brew cask install visual-studio-code
+brew cask install zoomus
