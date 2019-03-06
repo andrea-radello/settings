3e4665fa0a0a661a5d50cc584f5174a6da37308b
GibboK <gibbok@users.noreply.github.com>
2019-03-06 14:57:56 +0100

    Update install.sh

--- vscode/extensions/install.sh
+++ vscode/extensions/install.sh
@@ -3,12 +3,13 @@ code --list-extensions | xargs -L 1 echo code --install-extension
 
 # Install extensions
 code --install-extension cssho.vscode-svgviewer
+code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
-code --install-extension eg2.tslint
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension karigari.chat
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension ms-vsliveshare.vsliveshare-audio
 code --install-extension ms-vsliveshare.vsliveshare-pack
@@ -17,4 +18,3 @@ code --install-extension nikitaKunevich.snippet-creator
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-german
 code --install-extension vscodevim.vim
-code --install-extension DavidAnson.vscode-markdownlint
