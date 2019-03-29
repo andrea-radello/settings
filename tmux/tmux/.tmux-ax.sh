162bb62ff248c9b0c0d807a96a8fb69a3f8c3847
GibboK <gibbok@users.noreply.github.com>
2019-03-29 23:47:03 +0100

    Create .tmux-ax.sh

---
+++ tmux/tmux/.tmux-ax.sh
@@ -0,0 +1,20 @@
+# Replace XXX with your paths
+
+# Set environment variables
+COSMOS=~/XXX
+DEPLOYMENT_CONFIG=~/XXX
+# Create new session with first window named `ui`
+tmux new-session -s ax -n ui -d
+tmux split-window -h -t ax:ui.1
+tmux send-keys -t ax:ui.1 "cd $COSMOS/pond && nvm use" C-m
+tmux send-keys -t ax:ui.2 "cd $COSMOS/pond && nvm use" C-m
+tmux send-keys -t ax:ui.1 "clear" C-m
+tmux send-keys -t ax:ui.2 "clear" C-m
+# Create `devops` window
+tmux new-window -n devops -t ax
+tmux send-keys -t ax:devops.1 "cd $DEPLOYMENT_CONFIG" C-m
+tmux send-keys -t ax:devops.1 "clear" C-m
+# Select initial window and panel
+tmux select-window -t ax:ui.1
+tmux select-pane -t ax:ui.1
+tmux attach -t ax
