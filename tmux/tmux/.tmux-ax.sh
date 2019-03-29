4c151c31d3abb1030b6608563961406e3a8936b3
GibboK <gibbok@users.noreply.github.com>
2019-03-30 00:37:47 +0100

    Update .tmux-ax.sh

--- tmux/tmux/.tmux-ax.sh
+++ tmux/tmux/.tmux-ax.sh
@@ -4,18 +4,24 @@
 # Set environment variables
 COSMOS=~/XXX
 DEPLOYMENT_CONFIG=~/XXX
-# Create new session with first window named `ui`
-tmux new-session -s ax -n ui -d
-tmux split-window -h -t ax:ui.1
-tmux send-keys -t ax:ui.1 "cd $COSMOS/pond && nvm use" C-m
-tmux send-keys -t ax:ui.2 "cd $COSMOS/pond && nvm use" C-m
-tmux send-keys -t ax:ui.1 "clear" C-m
-tmux send-keys -t ax:ui.2 "clear" C-m
-# Create `devops` window
-tmux new-window -n devops -t ax
-tmux send-keys -t ax:devops.1 "cd $DEPLOYMENT_CONFIG" C-m
-tmux send-keys -t ax:devops.1 "clear" C-m
-# Select initial window and panel
-tmux select-window -t ax:ui.1
-tmux select-pane -t ax:ui.1
-tmux attach -t ax
+
+if tmux ls | grep attached -q ; then
+    echo cannot re create tmux session
+    exit 1
+else
+    # Create new session with first window named `ui`
+    tmux new-session -A -s ax -n ui -d
+    tmux split-window -h -t ax:ui.1
+    tmux send-keys -t ax:ui.1 "cd $COSMOS/pond && nvm use" C-m
+    tmux send-keys -t ax:ui.2 "cd $COSMOS/pond && nvm use" C-m
+    tmux send-keys -t ax:ui.1 "clear" C-m
+    tmux send-keys -t ax:ui.2 "clear" C-m
+    # Create `devops` window
+    tmux new-window -n devops -t ax
+    tmux send-keys -t ax:devops.1 "cd $DEPLOYMENT_CONFIG" C-m
+    tmux send-keys -t ax:devops.1 "clear" C-m
+    # Select initial window and panel
+    tmux select-window -t ax:ui.1
+    tmux select-pane -t ax:ui.1
+    tmux attach -t ax
+fi
