c735dd87577f838f52aea1c2257f0fd4bf9fc3c7
GibboK <gibbok@users.noreply.github.com>
2019-11-01 16:27:34 +0100

    Update install.sh

--- vscode/extensions/install.sh
+++ vscode/extensions/install.sh
@@ -2,24 +2,25 @@
 code --list-extensions | xargs -L 1 echo code --install-extension
 
 # Install extensions
+code --install-extension bungcip.better-toml
 code --install-extension cssho.vscode-svgviewer
 code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
-# code --install-extension esbenp.prettier-vscod
-# code --install-extension flowtype.flow-for-vscode
+code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension karigari.chat
-# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension ms-vsliveshare.vsliveshare-audio
 code --install-extension ms-vsliveshare.vsliveshare-pack
 code --install-extension naumovs.color-highlight
-# code --install-extension rust-lang.rust
-# code --install-extension scala-lang.scala
+code --install-extension rust-lang.rust
+code --install-extension scala-lang.scala
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-german
 code --install-extension vscodevim.vim
-code --install-extension dbaeumer.vscode-eslint
