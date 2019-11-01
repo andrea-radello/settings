dbad1ea2b2826c4981f6d4345509d2327daefe64
GibboK <gibbok@users.noreply.github.com>
2019-11-01 13:32:30 +0100

    Update install.sh

--- vscode/extensions/install.sh
+++ vscode/extensions/install.sh
@@ -7,14 +7,18 @@ code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
+# code --install-extension esbenp.prettier-vscod
+# code --install-extension flowtype.flow-for-vscode
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension karigari.chat
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+# code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension ms-vsliveshare.vsliveshare-audio
 code --install-extension ms-vsliveshare.vsliveshare-pack
 code --install-extension naumovs.color-highlight
+# code --install-extension rust-lang.rust
+# code --install-extension scala-lang.scala
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-german
 code --install-extension vscodevim.vim
