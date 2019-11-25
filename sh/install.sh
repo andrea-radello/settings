72529919af38532532e007957a7904809d85a843
GibboK <gibbok@users.noreply.github.com>
2019-11-25 14:05:27 +0100

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -1,6 +1,7 @@
 #  brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+brew install cask
 brew tap caskroom/cask
 brew tap caskroom/fonts
 
